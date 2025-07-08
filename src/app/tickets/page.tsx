1721beebb2d754c1392a6034fe8ce139709ae6c6
AbhayShaw1 <abhayshaw0301@gmail.com>
2025-07-08 22:08:17 +0530

    feat: animation to tickets page

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,11 +1,68 @@
 import Link from 'next/link';
 import { initialTickets } from '@/data';
 import { ticketPath } from '@/path';
+import clsx from 'clsx';
 
+const CHECK_ICON = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+            />
+        </svg>
+    );
+};
+
+const IN_PROGRESS_ICON = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m9 13.5 3 3m0 0 3-3m-3 3v-6m1.06-4.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
+            />
+        </svg>
+    );
+};
+
+const OPEN_ICON = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m20.893 13.393-1.135-1.135a2.252 2.252 0 0 1-.421-.585l-1.08-2.16a.414.414 0 0 0-.663-.107.827.827 0 0 1-.812.21l-1.273-.363a.89.89 0 0 0-.738 1.595l.587.39c.59.395.674 1.23.172 1.732l-.2.2c-.212.212-.33.498-.33.796v.41c0 .409-.11.809-.32 1.158l-1.315 2.191a2.11 2.11 0 0 1-1.81 1.025 1.055 1.055 0 0 1-1.055-1.055v-1.172c0-.92-.56-1.747-1.414-2.089l-.655-.261a2.25 2.25 0 0 1-1.383-2.46l.007-.042a2.25 2.25 0 0 1 .29-.787l.09-.15a2.25 2.25 0 0 1 2.37-1.048l1.178.236a1.125 1.125 0 0 0 1.302-.795l.208-.73a1.125 1.125 0 0 0-.578-1.315l-.665-.332-.091.091a2.25 2.25 0 0 1-1.591.659h-.18c-.249 0-.487.1-.662.274a.931.931 0 0 1-1.458-1.137l1.411-2.353a2.25 2.25 0 0 0 .286-.76m11.928 9.869A9 9 0 0 0 8.965 3.525m11.928 9.868A9 9 0 1 1 8.965 3.525"
+            />
+        </svg>
+    );
+};
 const TICKET_ICONS = {
-    DONE: 'D',
-    IN_PROGRESS: '>',
-    OPEN: 'O',
+    DONE: <CHECK_ICON />,
+    IN_PROGRESS: <IN_PROGRESS_ICON />,
+    OPEN: <OPEN_ICON />,
 };
 const TicketPage = () => {
     return (
@@ -14,7 +71,7 @@ const TicketPage = () => {
                 <h1 className="text-3xl font-bold tracking-tighter">Tickets Page</h1>
                 <p className="text-sm">Find All Your Tickets Here</p>
             </div>
-            <div className="flex flex-1 flex-col gap-y-4">
+            <div className="animate-fade-in-scale animate-fade-in-from-top flex flex-1 flex-col gap-y-4">
                 {initialTickets.map(ticket => {
                     return (
                         <div
@@ -23,7 +80,13 @@ const TicketPage = () => {
                         >
                             <div>{TICKET_ICONS[ticket.status]}</div>
                             <h3 className="text-lg font-semibold">{ticket.title}</h3>
-                            <p className="truncate text-sm text-slate-500">{ticket.content}</p>
+                            <p
+                                className={clsx('truncate text-sm text-slate-500', {
+                                    'line-through': ticket.status === 'DONE',
+                                })}
+                            >
+                                {ticket.content}
+                            </p>
                             <Link href={ticketPath(ticket.id)} className="text-sm underline">
                                 View Ticket
                             </Link>
