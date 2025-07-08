cc843c8e64d077ab7a98186ddc794f7041520898
AbhayShaw1 <abhayshaw0301@gmail.com>
2025-07-08 15:26:06 +0530

    feat: adding css to each tickets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,16 @@ import Link from 'next/link';
 
 const HomePage = () => {
     return (
-        <div>
-            <h1 className="text-lg">Home Page</h1>
-            <Link href={ticketsPath()} className="text-sm underline">
-                Go To Tickets Page
-            </Link>
+        <div className="flex flex-1 flex-col gap-y-8">
+            <div>
+                <h1 className="text-3xl font-bold tracking-tighter">Home Page</h1>
+                <p className="text-sm">Let&apos;s get started</p>
+            </div>
+            <div className="flex flex-1 flex-col items-center">
+                <Link href={ticketsPath()} className="text-sm underline">
+                    Go To Tickets Page
+                </Link>
+            </div>
         </div>
     );
 };
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,19 +1,36 @@
 import Link from 'next/link';
 import { initialTickets } from '@/data';
 import { ticketPath } from '@/path';
+
+const TICKET_ICONS = {
+    DONE: 'D',
+    IN_PROGRESS: '>',
+    OPEN: 'O',
+};
 const TicketPage = () => {
     return (
-        <div>
-            {initialTickets.map(ticket => {
-                return (
-                    <div key={ticket.id}>
-                        <h2 className="text-lg">{ticket.title}</h2>
-                        <Link href={ticketPath(ticket.id)} className="text-sm underline">
-                            View Ticket
-                        </Link>
-                    </div>
-                );
-            })}
+        <div className="flex flex-1 flex-col gap-y-8">
+            <div>
+                <h1 className="text-3xl font-bold tracking-tighter">Tickets Page</h1>
+                <p className="text-sm">Find All Your Tickets Here</p>
+            </div>
+            <div className="flex flex-1 flex-col gap-y-4">
+                {initialTickets.map(ticket => {
+                    return (
+                        <div
+                            key={ticket.id}
+                            className="max-w-[420px] rounded border border-slate-100 p-4"
+                        >
+                            <div>{TICKET_ICONS[ticket.status]}</div>
+                            <h3 className="text-lg font-semibold">{ticket.title}</h3>
+                            <p className="truncate text-sm text-slate-500">{ticket.content}</p>
+                            <Link href={ticketPath(ticket.id)} className="text-sm underline">
+                                View Ticket
+                            </Link>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
