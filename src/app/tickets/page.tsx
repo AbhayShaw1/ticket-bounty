536f57f99a011f8111505b501b06e8c8109571b8
AbhayShaw1 <abhayshaw01ab@gmail.com>
2025-07-09 13:36:21 +0530

    feat:simulate data fetching logic using sample data

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,13 +1,25 @@
+'use client';
 import { Heading } from '@/components/heading';
-import { initialTickets } from '@/data';
 import { TicketItem } from '@/features/ticket/components/ticket-item';
+import { getTicketsQuery } from '@/features/ticket/queries/get-tickets';
+import { Ticket } from '@/features/ticket/types';
+import { useEffect, useState } from 'react';
 
 const TicketPage = () => {
+    const [tickets, setTickets] = useState<Ticket[]>([]);
+
+    useEffect(() => {
+        const fetchTickets = async () => {
+            const ticketsResult = await getTicketsQuery();
+            setTickets(ticketsResult);
+        };
+        fetchTickets();
+    }, []);
     return (
         <div className="flex flex-1 flex-col gap-y-8">
             <Heading title="Tickets" description="Find All Your Tickets Here!" />
             <div className="animate-fade-in-from-top flex flex-1 flex-col gap-y-4">
-                {initialTickets.map(ticket => {
+                {tickets.map(ticket => {
                     return <TicketItem key={ticket.id} ticket={ticket} />;
                 })}
             </div>
