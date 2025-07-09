1f09f0a3b34fa5dc86608a220b9263cd7a7689aa
AbhayShaw1 <abhayshaw01ab@gmail.com>
2025-07-09 15:21:04 +0530

    refactor: data fetching in server component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,20 +1,9 @@
-'use client';
 import { Heading } from '@/components/heading';
 import { TicketItem } from '@/features/ticket/components/ticket-item';
 import { getTicketsQuery } from '@/features/ticket/queries/get-tickets';
-import { Ticket } from '@/features/ticket/types';
-import { useEffect, useState } from 'react';
 
-const TicketPage = () => {
-    const [tickets, setTickets] = useState<Ticket[]>([]);
-
-    useEffect(() => {
-        const fetchTickets = async () => {
-            const ticketsResult = await getTicketsQuery();
-            setTickets(ticketsResult);
-        };
-        fetchTickets();
-    }, []);
+const TicketPage = async () => {
+    const tickets = await getTicketsQuery();
     return (
         <div className="flex flex-1 flex-col gap-y-8">
             <Heading title="Tickets" description="Find All Your Tickets Here!" />
