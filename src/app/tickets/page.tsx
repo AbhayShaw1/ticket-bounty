cd75bc3f260aa3b516ec7bd79280a836032bd3b4
AbhayShaw1 <abhayshaw01ab@gmail.com>
2025-07-11 14:57:50 +0530

    feat: replace Card component with CardCompact for ticket creation

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,6 +1,6 @@
+import { CardCompact } from '@/components/card-compact';
 import { Heading } from '@/components/heading';
 import { Spinner } from '@/components/spinner';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { TicketCreateForm } from '@/features/ticket/components/ticket-create-form';
 import { TicketList } from '@/features/ticket/components/ticket-list';
 import { Suspense } from 'react';
@@ -9,15 +9,12 @@ const TicketPage = () => {
     return (
         <div className="flex flex-1 flex-col gap-y-8">
             <Heading title="Tickets" description="Find All Your Tickets Here!" />
-            <Card className="w-full max-w-[420px]">
-                <CardHeader>
-                    <CardTitle>Create New Ticket</CardTitle>
-                    <CardDescription>A new ticket will be generated here</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <TicketCreateForm />
-                </CardContent>
-            </Card>
+            <CardCompact
+                title="Create New Ticket"
+                className="w-full max-w-[420px] self-center"
+                description="A new ticket will be generated"
+                content={<TicketCreateForm />}
+            />
             <Suspense fallback={<Spinner />}>
                 <TicketList />
             </Suspense>
---
+++ src/components/card-compact.tsx
@@ -0,0 +1,22 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card';
+
+type CardCompactProps = {
+    title: string;
+    content: React.ReactNode;
+    description?: string;
+    className?: string;
+    footer?: React.ReactNode;
+};
+const CardCompact = ({ title, content, description, className, footer }: CardCompactProps) => {
+    return (
+        <Card className={className}>
+            <CardHeader>
+                <CardTitle>{title}</CardTitle>
+                <CardDescription>{description}</CardDescription>
+            </CardHeader>
+            <CardContent>{content}</CardContent>
+            {footer && <CardFooter>{footer}</CardFooter>}
+        </Card>
+    );
+};
+export { CardCompact };
