85fc8514c6cf1a1b8bf5f23f2a6f7c08bac78186
AbhayShaw1 <abhayshaw01ab@gmail.com>
2025-07-11 20:28:26 +0530

    add: loading spinner for create and edit form

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,13 +1,15 @@
-import { Button } from '@/components/ui/button';
+'use client';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { Ticket } from '../../../../generated/prisma';
 import { upsertTicketAction } from '../actions/upsert-ticket';
+import { SubmitButton } from '@/components/form/submit-button';
 
 type TicketUpsertFormProps = {
     ticket?: Ticket;
 };
+
 const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
     return (
         <form action={upsertTicketAction.bind(null, ticket?.id)} className="flex flex-col gap-y-2">
@@ -15,7 +17,7 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
             <Input type="text" name="title" id="title" defaultValue={ticket?.title} />
             <Label htmlFor="content">Content</Label>
             <Textarea name="content" id="content" defaultValue={ticket?.content} />
-            <Button type="submit">{ticket ? 'Edit Ticket' : 'Create Ticket'}</Button>
+            <SubmitButton label={ticket ? 'Edit Ticket' : 'Create Ticket'} />
         </form>
     );
 };
