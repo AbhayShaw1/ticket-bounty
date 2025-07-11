ba1a20ea6d879803a3c09795e3f4a57d7a73c63d
AbhayShaw1 <abhayshaw01ab@gmail.com>
2025-07-11 14:39:07 +0530

    feat: add ticket creation form to create ticket from ui

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,7 @@
 import { Heading } from '@/components/heading';
 import { Spinner } from '@/components/spinner';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { TicketCreateForm } from '@/features/ticket/components/ticket-create-form';
 import { TicketList } from '@/features/ticket/components/ticket-list';
 import { Suspense } from 'react';
 
@@ -7,6 +9,15 @@ const TicketPage = () => {
     return (
         <div className="flex flex-1 flex-col gap-y-8">
             <Heading title="Tickets" description="Find All Your Tickets Here!" />
+            <Card className="w-full max-w-[420px]">
+                <CardHeader>
+                    <CardTitle>Create New Ticket</CardTitle>
+                    <CardDescription>A new ticket will be generated here</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <TicketCreateForm />
+                </CardContent>
+            </Card>
             <Suspense fallback={<Spinner />}>
                 <TicketList />
             </Suspense>
--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -3,7 +3,7 @@ import { LucideLoader } from 'lucide-react';
 const Spinner = () => {
     return (
         <div className="flex flex-1 flex-col items-center justify-center self-center">
-            <LucideLoader className="h-16 w-16 animate-spin" />;
+            <LucideLoader className="h-16 w-16 animate-spin" />
         </div>
     );
 };
