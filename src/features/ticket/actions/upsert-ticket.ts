fa2c62b8a91336c9b2a0bc9a8b012275be38159b
AbhayShaw1 <abhayshaw01ab@gmail.com>
2025-07-12 12:33:22 +0530

    add: form validation via zod

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -2,24 +2,43 @@
 
 import { prisma } from '@/lib/prisma';
 import { ticketsPath } from '@/path';
+import { error } from 'console';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import z from 'zod';
 
-export const upsertTicketAction = async (ticketId: string | undefined, formData: FormData) => {
-    const data = {
-        title: formData.get('title') as string,
-        content: formData.get('content') as string,
-    };
-    await prisma.ticket.upsert({
-        where: {
-            id: ticketId || '',
-        },
-        update: data,
-        create: data,
-    });
+const upsertTicketSchema = z.object({
+    title: z
+        .string()
+        .min(1, 'Title is required')
+        .max(190, 'Title must be less than 255 characters'),
+    content: z.string().min(1).max(1024, 'Content must be less than 1024 characters'),
+});
+
+export const upsertTicketAction = async (
+    ticketId: string | undefined,
+    _actionState: { message: string },
+    formData: FormData,
+) => {
+    try {
+        const data = upsertTicketSchema.parse({
+            title: formData.get('title'),
+            content: formData.get('content'),
+        });
+        await prisma.ticket.upsert({
+            where: {
+                id: ticketId || '',
+            },
+            update: data,
+            create: data,
+        });
+    } catch (error) {
+        return { message: 'Error creating or updating ticket', error };
+    }
 
     revalidatePath(ticketsPath());
     if (ticketId) {
         redirect(ticketsPath());
     }
+    return { message: 'Ticket created successfully' };
 };
--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -5,19 +5,24 @@ import { Textarea } from '@/components/ui/textarea';
 import { Ticket } from '../../../../generated/prisma';
 import { upsertTicketAction } from '../actions/upsert-ticket';
 import { SubmitButton } from '@/components/form/submit-button';
+import { useActionState } from 'react';
 
 type TicketUpsertFormProps = {
     ticket?: Ticket;
 };
 
 const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
+    const [actionState, action] = useActionState(upsertTicketAction.bind(null, ticket?.id), {
+        message: '',
+    });
     return (
-        <form action={upsertTicketAction.bind(null, ticket?.id)} className="flex flex-col gap-y-2">
+        <form action={action} className="flex flex-col gap-y-2">
             <Label htmlFor="title">Title</Label>
             <Input type="text" name="title" id="title" defaultValue={ticket?.title} />
             <Label htmlFor="content">Content</Label>
             <Textarea name="content" id="content" defaultValue={ticket?.content} />
             <SubmitButton label={ticket ? 'Edit Ticket' : 'Create Ticket'} />
+            {actionState.message}
         </form>
     );
 };
