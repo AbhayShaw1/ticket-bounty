4b34fbd2ed9eff819b5a55c1aac296092b9ad917
AbhayShaw1 <abhayshaw01ab@gmail.com>
2025-07-12 12:33:48 +0530

    chore: removed unnecessary imports

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -2,7 +2,6 @@
 
 import { prisma } from '@/lib/prisma';
 import { ticketsPath } from '@/path';
-import { error } from 'console';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import z from 'zod';
